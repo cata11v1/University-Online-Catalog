3ba30f2de8397a8e55600e2a2a0368e30d0e94f4
Vrabiuta Catalin <94045391+cata11v1@users.noreply.github.com>
2023-03-08 00:19:23 +0200

    Delete Project directory

--- Project/Assistant.java
+++
@@ -1,5 +0,0 @@
-public class Assistant extends User{
-    public Assistant(String firstname, String lastname){
-        super(firstname, lastname);
-    }
-}
--- Project/Teacher.java
+++
@@ -1,5 +0,0 @@
-public class Teacher extends User{
-    public Teacher(String firstname, String lastname){
-        super(firstname, lastname );
-    }
-}
